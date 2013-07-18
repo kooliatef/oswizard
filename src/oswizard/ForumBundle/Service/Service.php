305d65dbe683f5d3e4fbb4dda7b3fc8cba1660b3
ilyes kooli <kooli.ilyes@gmail.com>
2013-07-18 13:25:50 +0300

    Update Service.php

--- src/oswizard/ForumBundle/Service/Service.php
+++ src/oswizard/ForumBundle/Service/Service.php
@@ -4,8 +4,23 @@ namespace oswizard\ForumBundle\Service;
 
 class Service {
 
-    public function findAllSections($forumController) {
-        $repository = $forumController->getDoctrine()
+    /**
+     *
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface 
+     */
+    protected $container;
+    
+    /**
+     * @param Container $container
+     * @param $docLink
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+    
+    public function findAllSections() {
+        $repository = $this->container->getDoctrine()
                 ->getManager()
                 ->getRepository('oswizardForumBundle:Section');
         return $repository->findAll();
